da084180b7e3402cf0f24eeda39f5e03d6c9e165
Dhanush <dhanushvenkat890@gmail.com>
2025-01-25 11:55:26 +0530

    Starting sample project

---
+++ src/main/java/com/DPT/RTTI/RttiApplication.java
@@ -0,0 +1,14 @@
+package com.DPT.RTTI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RttiApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(RttiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DPT/RTTI/RttiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DPT.RTTI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RttiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
